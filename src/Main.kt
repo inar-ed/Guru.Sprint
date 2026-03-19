0d5fedc20c2e287c6f7aa5edb47dcc48096fde6d
User 1 <inar85160@gmail.com>
2026-03-19 09:07:32 +0300

    second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
     val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-    println("Hello, Nurgali Aslanovich" )
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("Hello, Nurgali Aslanovich")
+
+    val a: Int = 12
+    val b: String = " "
+    val c: Boolean
+
+    println("Сделал")
+
 }
