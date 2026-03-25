a0b53790763122d3a654e85044d59c23ffa74486
User 1 <inar85160@gmail.com>
2026-03-25 14:49:11 +0300

    GS-2-2

--- src/lesson2/lesson2_task1.kt
+++ src/lesson2/lesson2_task1.kt
@@ -3,17 +3,34 @@ package lesson2
 const val AGE_OF_MAJORITY = 18
 
 fun main() {
-    println("Enter your age: ")
-    val userAge = readln()
-    val age = userAge.toIntOrNull() ?: 0
+//    println("Enter your age: ")
+//    val userAge = readln()
+//    val age = userAge.toIntOrNull() ?: 0
+//
+//    val result: Boolean = age >= AGE_OF_MAJORITY
+//
+//    if (result) {
+//        println("You are old enough")
+//    } else {
+//        println("You are too young")
+//    }
+//
+//    println(result)
 
-    val result: Boolean = age >= AGE_OF_MAJORITY
+    println("Введите возраст : ")
+    val userAge2 = readln()
+    val age2 = userAge2.toIntOrNull() ?: 0
 
-    if (result) {
-        println("You are old enough")
+    val result2: Boolean = age2 >= AGE_OF_MAJORITY
+
+    if (result2) {
+        println("Добро пожаловать!")
     } else {
-        println("You are too young")
+        println("Иди гуляй! Не дорос еще Алихан")
     }
 
-    println(result)
+    println(result2)
+    
+    
+    
 }
